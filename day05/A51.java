588a9f315d05234c419adc4633a9c7f963144fae
JiYeon <50862497+JYn0@users.noreply.github.com>
2019-07-23 09:10:57 +0900

    프로그래머스

---
+++ day05/A51.java
@@ -0,0 +1,22 @@
+package day05;
+// 완주하지 못한 선수
+import java.util.Arrays;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int numc = completion.length; 
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int num=0;
+        for(int i=0; i<numc; i++) {
+            if(participant[i].equals(completion[i])) {
+                num ++;
+            }else{
+                break;
+            }
+        }
+        answer = participant[num];   
+        return answer;
+    }
+}
