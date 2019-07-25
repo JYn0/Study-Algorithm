0e28134fb30f63cb3d310994ee62068dccefe343
JiYeon <50862497+JYn0@users.noreply.github.com>
2019-07-25 16:25:31 +0900

    프로그래머스

--- day06/A61.java
+++ day06/A61.java
@@ -1,40 +1,4 @@
-package day06;
-
-import java.util.Arrays;
-
-public class A61 {
-	public static void main(String[] args) {
-
-		int[] arr = { 1, 1, 3, 3, 0, 1, 1, 5,5,7 };
-		
-		int cnt = 0;
-		int arrsize = arr.length;
-		
-		for (int i = 1; i < arrsize; i++) {
-			if (arr[i-1] == arr[i]) {
-				cnt++;
-			}
-		}
-		
-		int abc = arrsize-cnt;
-		int[] answer = new int[abc];
-		int k=0;
-		
-		for(int i=0; i<arrsize-1; i++) {
-			if(arr[i] != arr[i+1]) {
-				answer[k] = arr[i];
-				k++;
-			}
-		}
-		
-		answer[abc-1] = arr[arrsize-1];
-		System.out.print(Arrays.toString(answer));
-	}
-}
-
-/*
-
-// °°Àº ¼ýÀÚ´Â ½È¾î
+//  ê°™ì€ ìˆ«ìžëŠ” ì‹«ì–´
 
 import java.util.*;
 
@@ -64,4 +28,3 @@ public class Solution {
         return answer;
 	}
 }
-*/
