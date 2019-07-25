989b4b50c010433f403121e6c6bb3a19bb560b07
JiYeon <50862497+JYn0@users.noreply.github.com>
2019-07-25 16:26:10 +0900

    프로그래머스

--- day06/A62.java
+++ day06/A62.java
@@ -1,38 +1,5 @@
-package day06;
 
-public class A62 {
-
-	public static void main(String[] args) {
-		int a = 5;
-		int b = 7;
-		long answer = 0;
-		int size=0;
-	    
-		if(a > b){
-	    	size = a - b ;
-	        for(int i=0; i<size; i++){
-	        	answer += b;
-	        	b++;
-	        }
-	    } else if(a < b){
-	    	size = b - a;
-	    	for(int i=0; i<=size; i++){
-		        answer += a;
-		        a++;
-		        System.out.println(answer);
-	    	}
-	    } else{
-	    	answer = a;
-	    }
-	   
-	    System.out.println(answer);
-	}
-
-}
-
-/*
- 
-//µÎ Á¤¼ö »çÀÌÀÇ ÇÕ
+// ë‘ ì •ìˆ˜ ì‚¬ì´ì˜ í•©
 
 class Solution {
   public long solution(int a, int b) {
@@ -57,4 +24,3 @@ class Solution {
       return answer;
   }
 }
-*/
